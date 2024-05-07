7d6027b6f2014d973b69f65963b4ca8699e1a337
reeves-48777 <durandthomas@protonmail.com>
2024-05-08 00:25:41 +0200

    fix - cursor component

--- src/components/Cursor.vue
+++ src/components/Cursor.vue
@@ -1,25 +1,28 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, watch } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
+import { useGlobalStore } from '../store';
 import { gsap } from 'gsap';
+
+const store = useGlobalStore();
 
 const outer = ref<HTMLElement | null>(null);
 const inner = ref<HTMLElement | null>(null);
+const position = ref({x: 0, y: 0});
 
 const outerDuration = .4;
 const innerDuration = .2;
 const colorChangeDuration = .06;
 
-const handleMouseMove = (event: MouseEvent) => {
-    if (outer.value && inner.value) {
+let rafId: number;
 
+const updateCursorPosition = () => {
+    if (outer.value && inner.value) {
         const outerRect = outer.value.getBoundingClientRect();
         const innerRect = inner.value.getBoundingClientRect();
 
-        const cursor = { x: event.clientX, y: event.clientY };
-        const outerPosition = {x: cursor.x - outerRect.width/2, y: cursor.y - outerRect.height/2}
-        const innerPosition = {x: cursor.x - innerRect.width/2, y: cursor.y - innerRect.height/2};
+        const outerPosition = {x: position.value.x - outerRect.width/2, y: position.value.y - outerRect.height/2};
+        const innerPosition = {x: position.value.x - innerRect.width/2, y: position.value.y - innerRect.height/2};
 
-        // outer part
         gsap.to(outer.value, {
             duration: outerDuration,
             ease: 'power1.out',
@@ -36,45 +39,48 @@ const handleMouseMove = (event: MouseEvent) => {
             top: innerPosition.y,
         });
 
-        const below = document.elementFromPoint(cursor.x, cursor.y);
-        if (below?.classList.contains('mod-cursor')) {
+        if (store.modCursor) {
             handleMouseOver();
         } else {
             handleMouseOut();
         }
     }
+    rafId = requestAnimationFrame(updateCursorPosition);
+}
+
+const handleMouseMove = (event: MouseEvent) => {
+    position.value = {x: event.clientX, y: event.clientY};
+    if (!rafId) {
+        rafId = requestAnimationFrame(updateCursorPosition);
+    }
 };
 
 const handleMouseOver = () => {
-    if (outer.value) {
-        gsap.to(outer.value, {
-            scale: 1.5,
-            outlineColor: 'black',
-            duration: colorChangeDuration
-        });
+    gsap.to(outer.value, {
+        scale: 1.5,
+        outlineColor: 'black',
+        duration: colorChangeDuration
+    });
 
-        gsap.to(inner.value, {
-            backgroundColor: 'black',
-            scale: 1.2,
-            duration: colorChangeDuration
-        });
-    }
+    gsap.to(inner.value, {
+        backgroundColor: 'black',
+        scale: 1.2,
+        duration: colorChangeDuration
+    });
 };
 
 const handleMouseOut = () => {
-    if (outer.value) {
-        gsap.to(outer.value, {
-            scale: 1,
-            outlineColor: 'white',
-            duration: colorChangeDuration
-        });
+    gsap.to(outer.value, {
+        scale: 1,
+        outlineColor: 'white',
+        duration: colorChangeDuration
+    });
 
-        gsap.to(inner.value, {
-            backgroundColor: 'white',
-            scale: 1,
-            duration: colorChangeDuration
-        });
-    }
+    gsap.to(inner.value, {
+        backgroundColor: 'white',
+        scale: 1,
+        duration: colorChangeDuration
+    });
 }
 
 onMounted(() => {
@@ -83,6 +89,9 @@ onMounted(() => {
 
 onUnmounted(() => {
     window.removeEventListener('mousemove', handleMouseMove);
+    gsap.killTweensOf(outer.value);
+    gsap.killTweensOf(inner.value);
+    // cancelAnimationFrame(rafId);
 });
 
 
@@ -95,7 +104,7 @@ onUnmounted(() => {
 
 <style scoped>
     .outer, .inner {
-        @apply fixed rounded-full origin-center z-50
+        @apply fixed rounded-full origin-center pointer-events-none z-50
     }    
     .outer {
         @apply bg-transparent size-[8px] outline outline-2 outline-offset-[6px] outline-white left-1/2 top-1/2;
